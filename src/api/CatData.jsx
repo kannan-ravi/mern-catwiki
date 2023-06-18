6832a43d6205a424842df93b0e87c34fefbae70f
Kannan <kannanravindran00kr@gmail.com>
2023-06-18 19:24:02 +0530

    Errors rectified

--- src/api/CatData.jsx
+++ src/api/CatData.jsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000' || "https://image-uploader-zv1t.onrender.com"
+  baseURL: 'http://localhost:3000' || "https://catwiki-kannan-api.onrender.com"
 });
-=======
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: "https://catwiki-kannan-api.onrender.com"
-});
->>>>>>> 92c55fd8f1475864a18e3f83a16ec8b87b37718a
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './Footer.css';
 import Logo from '../../assets/CatwikiLogo.svg';
 
@@ -13,20 +12,4 @@ const Footer = () => {
   )
 }
 
-=======
-import './Footer.css';
-import Logo from '../../assets/CatwikiLogo.svg';
-
-import { Link } from 'react-router-dom';
-
-const Footer = () => {
-  return (
-    <footer className='footer--container'>
-      <Link to={'/'}><img src={Logo} alt='' className='footer--image' /></Link>
-      <p className='footer--text'>&#169; created by <span><a href="https://github.com/kannan-ravi?tab=repositories">kannan-ravi</a></span> - devChallenge.io 2023</p>
-    </footer>
-  )
-}
-
->>>>>>> 92c55fd8f1475864a18e3f83a16ec8b87b37718a
 export default Footer
